161612fe01f1f54fbd0f3fc8c3afafc694bdde9b
megakoko <megadestroyer007@gmail.com>
2018-10-09 18:01:13 +0400

    Initial commit

---
+++ cms/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  cms
+//
+//  Created by Andy on 09/10/2018.
+//  Copyright © 2018 Andy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
