9fdd7f3ff0c9b5daa7ad67ec839ebf88c62dcd21
megakoko <megadestroyer007@gmail.com>
2018-10-13 14:22:55 +0400

    Use global tint color

--- cms/Client/ClientViewController.swift
+++ cms/Client/ClientViewController.swift
@@ -102,9 +102,6 @@ class ClientViewController : UITableViewController {
             cell.selectionStyle = .none
             cell.textView.text = fields[indexPath.section].data
             cell.textView.dataDetectorTypes = fields[indexPath.section].detectorTypes
-            if cell.textView.dataDetectorTypes != UIDataDetectorTypes() {
-                cell.textView.tintColor = navigationController?.navigationBar.tintColor
-            }
         }
 
         return cell
