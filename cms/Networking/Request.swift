f0427e8b1f021cd4d0be4e485b9fac76dcb090c3
megakoko <megadestroyer007@gmail.com>
2018-11-15 17:10:27 +0400

    Implemented network layer

---
+++ cms/Networking/Request.swift
@@ -0,0 +1,9 @@
+//
+//  Request.swift
+//  cms
+//
+//  Created by Andy on 14/11/2018.
+//  Copyright © 2018 Andy. All rights reserved.
+//
+
+import Foundation
---
+++ cms/Networking/Response.swift
@@ -0,0 +1,9 @@
+//
+//  Response.swift
+//  cms
+//
+//  Created by Andy on 14/11/2018.
+//  Copyright © 2018 Andy. All rights reserved.
+//
+
+import Foundation
