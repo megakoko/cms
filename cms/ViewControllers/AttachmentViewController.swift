ffdc458df157c2a3ea4ebee6420a53e768dfe7a1
Andy <megadestroyer007@gmail.com>
2018-11-25 15:34:39 +0400

    Save attachment to temporary file and display that

--- cms/ViewControllers/AttachmentViewController.swift
+++ cms/ViewControllers/AttachmentViewController.swift
@@ -23,33 +23,26 @@ class AttachmentViewController: UIViewController {
             if let error = response?.error {
                 print("Failed to get attachmnet: \(error)")
             } else {
-                let attachment = response?.parsed(Attachment.self)
-
-                guard let base64string = attachment?.file?.replacingOccurrences(of: "\n", with: "", options: .literal, range: nil),
-                      let base64 = Data(base64Encoded: base64string) else {
+                guard let attachment = response?.parsed(Attachment.self) else {
                     print("Failed to parse attachment")
                     return
                 }
 
-                guard let lastDotIndex = attachment?.fileName.lastIndex(of: "."),
-                      let suffix = attachment?.fileName.suffix(from: lastDotIndex) else {
-                    print("Can't determine attachment suffix")
+                guard let base64string = attachment.file?.replacingOccurrences(of: "\n", with: "", options: .literal, range: nil),
+                      let data = Data(base64Encoded: base64string) else {
+                    print("Failed to parse attachment")
                     return
                 }
 
-                var mimeType = ""
-                switch suffix {
-                case ".pdf":
-                    mimeType = "application/pdf"
-                case ".xhtml":
-                    mimeType = "application/xhtml"
-                default:
-                    print("Invalid attachment format: \(suffix)")
+                let attachmentUrl = FileManager.default.temporaryDirectory.appendingPathComponent(attachment.fileName)
+
+                guard let _ = try? data.write(to: attachmentUrl) else {
+                    print("Failed to save attachment to temporary file: \(attachmentUrl)")
                     return
                 }
 
                 DispatchQueue.main.async {
-                    self.webView!.load(base64, mimeType: mimeType, characterEncodingName: "", baseURL: URL(string: "http://localhost")!)
+                    self.webView.loadFileURL(attachmentUrl, allowingReadAccessTo: attachmentUrl)
                 }
             }
         }
